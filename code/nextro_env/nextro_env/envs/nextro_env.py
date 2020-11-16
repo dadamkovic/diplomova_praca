19802bf8b910c0a85d03ea1a25f5fa90a2863aa3
Daniel <dadamkovic@gmail.com>
2020-11-16 23:48:20 +0100

    Changes to reward, render, observation, action spaces. Added more comprehensive options to minitaur inspired agent and parsing of command line arguments. Reward function still not complete, PD regulators not added to motors.

--- code/nextro_env/nextro_env/envs/nextro_env.py
+++ code/nextro_env/nextro_env/envs/nextro_env.py
@@ -13,7 +13,7 @@ import pybullet as p
 from pybullet_envs import robot_bases as rb
 import pybullet_data
 import numpy as np
-
+from collections import deque
 
 #the urdf location has to be absolute path
 NEXTRO_LOC = __file__.replace('envs/nextro_env.py','assets/urdf/nextro.urdf')
@@ -21,6 +21,18 @@ NEXTRO_LOC = __file__.replace('envs/nextro_env.py','assets/urdf/nextro.urdf')
 DEFAULT_MAX_TIME = 20
 #defines the FPS that the simulation will run at
 FRAMES_PER_SECOND = 30
+
+NUM_JOINTS = 18
+#(NUM_JOINTS joint angles + NUM_JOINTS joint velocities + yaw + pitch + roll)
+CURRENT_SENZOR_OUTPUT_SIZE = (NUM_JOINTS*2 + 3)
+#((NUM_JOINTS old joint angles + NUM_JOINTS old joint velocities + NUM_JOINTS previous joint angles) +
+#(yaw + pitch + roll))
+STORED_OBSERVATION_SIZE = NUM_JOINTS*3 + 3
+#4 previous moves
+#TODO: maybe set them spaced 2 frames from each other
+OBSERVATION_SIZE = CURRENT_SENZOR_OUTPUT_SIZE + STORED_OBSERVATION_SIZE*4
+#number of motorized joints on a robot
+
 
 JOINT_MOVEMNT_COST_DISCOUNT = 0.2
 
@@ -75,11 +87,14 @@ class NextroBot(rb.URDFBasedRobot):
     def calc_state(self):
         return 0
 
-
 #defines all the methods that a gym environment has to provide
 class NextroEnv(gym.Env):
-    def __init__(self, render=False, episode_length=DEFAULT_MAX_TIME):
-        self.robot = NextroBot(NEXTRO_LOC, 'nextro', 36, 18, basePosition=[0,0,0.1],
+    def __init__(self):
+        self.robot = NextroBot(model_urdf=NEXTRO_LOC,
+                               robot_name='nextro',
+                               action_dim=NUM_JOINTS,
+                               obs_dim=OBSERVATION_SIZE,
+                               basePosition=[0,0,0.1],
                                baseOrientation=[0,0,0,1])
 
         self.action_space = self.robot.action_space
@@ -92,27 +107,31 @@ class NextroEnv(gym.Env):
         self.reward = None
         self.done = True
         self._info = None
+        self.render_env = None
 
         #set true after reset, set to false after episode ends
         self._init = False
         self._time_step = 1/FRAMES_PER_SECOND
         self.__name__ = 'Nextro'
-        #used when calculating reward
-        self._old_position = []
-        self._episode_length = episode_length
+        self._episode_length = DEFAULT_MAX_TIME
         #keeps track of how long an episode has been runnning
         self._time_elapsed = 0
         self._old_dist_travelled = 0
-
         self.client = None
-
-
-
+        self.new_observation = np.zeros(CURRENT_SENZOR_OUTPUT_SIZE)
+        self.obs_buffer = deque(
+            np.zeros(STORED_OBSERVATION_SIZE) for i in range(4))
 
     #reset is called when first creating the env and after each episode ends
     def reset(self):
         self._init = True
         self.done = False
+
+        if self.render_env == None:
+            self._time_delay = False
+            self.client = p.connect(p.DIRECT)
+            p.setAdditionalSearchPath(pybullet_data.getDataPath())
+            self.render_env = False
 
         #if the robot is loaded we only need to move it not add it again
         if self.robot.robot_body is not None:
@@ -121,45 +140,68 @@ class NextroEnv(gym.Env):
             self._reset_joints()
         else:
             p.resetSimulation(self.client)
-            p.setTimeStep(self._time_step,self.client)
-            p.setGravity(0,0,-10)
+            p.setTimeStep(self._time_step, self.client)
+            p.setGravity(0, 0, -10)
             p.loadURDF("plane.urdf")
             self.robot.reset(p)
             #see above how these are used
             self._original_position = self.robot.robot_body.get_position()
             self._original_orientation = self.robot.robot_body.get_orientation()
+            #move camera closer to robot
             p.resetDebugVisualizerCamera(1.2, -145, -38, [0,0,0])
             p.setTimeStep(self._time_step, self.client)
         #reset episode timer
         self._time_elapsed = 0
         #reset baseline position
         self._old_dist_travelled = 0
-
-        #TODO: change this to non constant later
-        self._old_joint_states = np.zeros(18)
-        self.state = self._getstate()
+        self.obs_buffer = deque(
+            np.zeros(STORED_OBSERVATION_SIZE) for i in range(4))
+        self.state = self._get_state()
         return self.state
+
+
+    #no need for this method
+    def render(self, **kwargs):
+        if self.client is not None:
+            #will make sure that in human render mode camera will follow robot
+            if self.render_env:
+                x, y, _ = self.robot.robot_body.get_position()
+                p.resetDebugVisualizerCamera(1.2, -145, -38, [x ,y ,0])
+            return
+        if kwargs['mode'] == 'human':
+            self.render_env = True
+            self.client = p.connect(p.GUI)
+            self._time_delay = True
+        #TODO:stuff bellow could be potentially removed
+        else:
+            self._time_delay = False
+            self.client = p.connect(p.DIRECT)
+            self.render_env = False
+        #used by loadURDF(plane.urdf)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        return
+
 
     def step(self, action):
         if not self._init:
             raise Exception('Initialize the environment first!')
         if self.done:
             raise Exception('Reset environment after episode ends!')
-
-        angles = []
-
-        #joint angles are set using the gaussian distribution
-        #actions are are paired (mu1, sigma1, mu2, sigma2,...) total 18*2 items
-        for idx in range(0, self.action_space.shape[0], 2):
-            angles.append(np.random.default_rng().normal(action[idx],
-                                                         abs(action[idx+1]),
-                                                         None))
-        self._set_joints(angles)
+        #TDOD: Here I could potentially add PD regulator
+        #
+        #
+        #clipping to make sure we don't set the motors to weird angles
+        action = np.clip(action, a_min=-1.4, a_max=1.4)
+        self._set_joints(action)
         self._time_elapsed += self._time_step
-        self.state = self._getstate()
-        self.reward = self._get_reward_update_done()
-        p.stepSimulation()
 
+        self.state = self._get_state()
+        self.reward = self._get_reward_update_done()
+
+        p.stepSimulation()
+        self._update_buffer(action)
+
+        #time delay only set in human render mode
         if self._time_delay:
             time.sleep(self._time_step)
 
@@ -169,27 +211,30 @@ class NextroEnv(gym.Env):
       self.np_random, seed = seeding.np_random(seed)
       return [seed]
 
+
+    def _update_buffer(self, action):
+        obs_2_store = np.concatenate((self.new_observation, action))
+        self.obs_buffer.pop()
+        self.obs_buffer.appendleft(obs_2_store)
+
+
     #the reward is calculated as postion change in 2D space (x, y axis)
     #plus the discounted total distance from starting position
     def _get_reward_update_done(self):
         x, y, z, a, b, c, d = self.robot.robot_body.get_pose()
-        angle = p.getEulerFromQuaternion((a, b, c, d))
+        yaw, pitch, roll = p.getEulerFromQuaternion((a, b, c, d))
         #original position was not exactly [0,0] so adjust current coordinates
         x = x - self._original_position[0]
         y = y - self._original_position[1]
-        #distance from the origin (x, y axis)
-        self._new_dist_travelled = np.sqrt(x**2+y**2)
-
+        #euclidean distance from the origin (x, y axis)
+        new_dist_travelled = np.linalg.norm([x, y])
         reward = -1
-        if self._new_dist_travelled > self._old_dist_travelled:
+        if new_dist_travelled > self._old_dist_travelled:
             reward = 1
-        self._old_dist_travelled = self._new_dist_travelled
-        current_joint_states = self._getstate()
-        reward = reward - JOINT_MOVEMNT_COST_DISCOUNT*np.sum(np.abs(current_joint_states-self._old_joint_states))
-        self._old_joint_states = current_joint_states
+            self._old_dist_travelled = new_dist_travelled
 
         #if the robot turns over end episode and give bad reward
-        if abs(angle[0]) > (np.pi/2):
+        if abs(yaw) > (np.pi/2):
             self.done = True
             self._init = False
             self._time_elapsed = 0
@@ -202,33 +247,30 @@ class NextroEnv(gym.Env):
 
         return reward
 
-    #no need for this method
-    def render(self, **kwargs):
-        if self.client is not None:
-            if kwargs['mode'] == 'human':
-                x, y, _ = self.robot.robot_body.get_position()
-                p.resetDebugVisualizerCamera(1.2, -145, -38, [x,y,0])
-            return
-        if kwargs['mode'] == 'human':
-            self.client = p.connect(p.GUI)
-            self._time_delay = True
-        else:
-            self._time_delay = False
-            self.client = p.connect(p.DIRECT)
-        #used by loadURDF(plane.urdf)
-        p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        return
 
     #get the current angle of every joint and return them as numpy array
-    def _getstate(self):
+    def _get_state(self):
         if not self._init:
             raise Exception('Initialize the environment first')
+        _, _, _, a, b, c, d = self.robot.robot_body.get_pose()
+        body_angles = p.getEulerFromQuaternion((a, b, c, d))
         joint_angles = []
-        for joint in JOINT_NAMES:
-            joint_angles.append(self.robot.jdict[joint].get_position())
-        return np.array(joint_angles)
+        joint_velocities = []
 
-    #all joints reset to 0 position
+        for joint in JOINT_NAMES:
+            joint_handle = self.robot.jdict[joint]
+            joint_angles.append(joint_handle.get_position())
+            joint_velocities.append(joint_handle.get_velocity())
+
+        self.new_observation = np.concatenate((joint_angles,
+                                               joint_velocities,
+                                               body_angles))
+        unrolled_buffer = np.ndarray.flatten(np.array(self.obs_buffer))
+
+        return np.concatenate((self.new_observation, unrolled_buffer))
+
+
+    #all joints reset to 0 position, overrides simulation constraints
     def _reset_joints(self):
         for joint in JOINT_NAMES:
             self.robot.jdict[joint].reset_position(0, 0)
--- code/nextro_training/minitaur_inspired_agent/train_minitaur_inspired.py
+++ code/nextro_training/minitaur_inspired_agent/train_minitaur_inspired.py
@@ -10,17 +10,84 @@ Created on Sat Oct 31 11:27:59 2020
 
 from all.experiments import SingleEnvExperiment
 from all.environments import GymEnvironment
-import all
 import nextro_env
 from sac_minitaur_inspired import sac_minitaur_inspired
+from argparse import ArgumentParser
+import os
+from torch import load as t_load
+
+NET_FILES = ['policy.pt', 'q_1.pt', 'q_2.pt','v.pt']
+USAGE = """"The program launches the training of an agent in either pybullet GUI
+            or DIRECT mode."""
+parser = ArgumentParser(usage=USAGE)
+
+def resolve_arguments():
+
+    parser.add_argument('--render', default=False, action='store_true',
+                        help="Set this flag to render the environment.")
+
+    parser.add_argument('--device', default='cuda', required=False,
+                        type=str, choices = ['cuda', 'cpu'],
+                        help="Can be either 'cuda' (default) or 'cpu', will determine if pytorch can use CUDA cores.")
+
+#TODO: that latest thing isnt implemented yet but it would be cool to do it eventually
+    parser.add_argument('--regime', default='train', required=False,
+                        type=str, choices = ['train', 'test'],
+                        help="Determines if agent starts training or just test a pretrained network. " +
+                        "Both training and testing can pre-load a trained network if its location is specified " +
+                        "using the -loc flag.")
+
+    parser.add_argument('--loc', default='', type=str, required=False,
+                        help="When loading a pretrained network specify its location directory here " +
+                        "(needs to be set when using -regime 'test'). Use 'latest' when using the " +
+                        "latest network in runs dir. Files that hold network weights must be named " +
+                        "q_1.pt, q_2.pt, policy.pt, v.pt")
+
+    parser.add_argument('--episodes', type=int, required=True,
+                        help="Determines the numer of training/testing episodes. ")
+
+    args = parser.parse_args()
+    if args.episodes < 1:
+        Exception("Specify number of episodes at least 1!")
+    return args
+
+def load_weights(location, exp):
+    try:
+        filenames = os.listdir(location)
+    except FileNotFoundError():
+        raise Exception("You have to specify correct directory path!")
+
+    for net_file in NET_FILES:
+        if net_file not in filenames:
+            raise Exception("The location you specified does not include the network files. " +
+                          "Use --help for more info.")
 
 
+    nn_paths = []
+    for weight_file in NET_FILES:
+        nn_paths.append(os.path.join(location, weight_file))
 
-device = 'cuda'
-env = GymEnvironment('nextro-v0', device)
-env.render(mode='train')
-env.reset()
-agent = sac_minitaur_inspired(device=device)
-exp = SingleEnvExperiment(agent, env)
+    #TODO: figure out how this plays with the CUDA settings, until then hope it works
+    exp._agent.agent.policy.model = t_load(nn_paths[0])
+    exp._agent.agent.q_1.model = t_load(nn_paths[1])
+    exp._agent.agent.q_2.model = t_load(nn_paths[2])
+    exp._agent.agent.v.model = t_load(nn_paths[3])
+    print('----------------------------')
+    print("WEIGHTS LOADED!")
+    print('----------------------------')
+    return exp
 
-exp.train(episodes=10)
+if __name__ == '__main__':
+    args = resolve_arguments()
+
+    env = GymEnvironment('nextro-v0', args.device)
+    agent = sac_minitaur_inspired(device=args.device)
+    exp = SingleEnvExperiment(agent, env, render=args.render)
+
+    if args.loc != '':
+        exp = load_weights(args.loc, exp)
+
+    if args.regime == 'train':
+        exp.train(episodes=args.episodes)
+    else:
+        exp.test(episodes=args.episodes)
